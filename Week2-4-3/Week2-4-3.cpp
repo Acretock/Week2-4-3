be35e81636c17e126a69e558a3580f6b25d68ab5
Acretock <georgoncharov@gmail.com>
2021-04-20 19:06:27 +0500

    V1.0

--- Week2-4-3/Week2-4-3.cpp
+++ Week2-4-3/Week2-4-3.cpp
@@ -1,20 +1,53 @@
-// Week2-4-3.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+#include <set>
+#include <map>
+#include <vector>
+#include <sstream>
+#include <string>
 #include <iostream>
+using namespace std;
+
+void Add(set<pair<string,string>>& res, string word1, string word2) {
+	res.insert({{word1},{word2}});
+}
+
+void Count(set<pair<string, string>>& res, string word) {
+	int c = 0;
+	for (auto i : res) {
+		if ((i.first == word) || (i.second == word))
+			c++;
+	}
+	cout << c << endl;
+}
+
+void Check(set<pair<string, string>>& res, string word1, string word2) {
+	if ((res.count({word1,word2}) != 0)|| (res.count({ word2,word1 }) != 0))
+		cout << "YES" << endl;
+	else
+		cout << "NO" << endl;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	int N=100;
+	string line, command, str1, str2;
+	set<pair<string, string>> res;
+	cin >> N;
+	for (int i = 0; i <= N; i++) {
+		getline(cin, line);
+		stringstream ss(line);
+		ss >> command;
+		ss >> str1;
+		if (ss.good())
+			ss >> str2;
+		if (command == "ADD") {
+			Add(res, str1, str2);
+		}
+		if (command == "COUNT") {
+			Count(res, str1);
+		}
+		if (command == "CHECK") { 
+			Check(res, str1, str2);
+		}
+
+	}
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
